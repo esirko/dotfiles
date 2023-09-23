10b690f724fd22673df12179f0a98eb292c180d9
Edwin Sirko <esirko@github.com>
2023-09-23 13:13:56 -0400

    zsh prompt etc

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -20,9 +20,9 @@ git_dirty() {
   else
     if [[ $($git status --porcelain) == "" ]]
     then
-      echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
+      echo "%{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
     else
-      echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
+      echo "%{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
     fi
   fi
 }
@@ -51,7 +51,8 @@ need_push () {
 }
 
 directory_name() {
-  echo "%{$fg_bold[cyan]%}%1/%\/%{$reset_color%}"
+  #echo "%{$fg_bold[blue]%}%1/%\/%{$reset_color%}"
+  echo "%{$fg_bold[blue]%}%1/%{$reset_color%}"
 }
 
 battery_status() {
@@ -66,12 +67,116 @@ battery_status() {
   fi
 }
 
-export PROMPT=$'\n$(battery_status)in $(directory_name) $(git_dirty)$(need_push)\n› '
+colorized_exit_code()
+{
+  if [ $LastExitCodeValue = "0" ]; then
+    echo "%{$fg_bold[green]%}$LastExitCodeValue%{$reset_color%}"
+  else
+    echo "%{$fg_bold[red]%}$LastExitCodeValue%{$reset_color%}"
+  fi
+}
+
+GDATE_EXISTS=1 # If gdate doesn't exist, either turn this off or install gdate with: brew install coreutils
+# https://stackoverflow.com/questions/1862510/how-can-the-last-commands-wall-time-be-put-in-the-bash-prompt/1862762#1862762
+# https://stackoverflow.com/questions/2704635/is-there-a-way-to-find-the-running-time-of-the-last-executed-command-in-the-shel
+#export timer_start=$(timer_now) # Initialize
+
+
+timer_now() {
+#  if [ "$UNAME" = "Darwin" ]; then
+    if [ -n "$GDATE_EXISTS" ]; then
+      gdate +%s%N
+    else
+      date +%s000000000
+    fi
+#  else #elif [ "$UNAME" == "Linux" ]; then
+#    date +%s%N
+#  fi
+}
+
+timer_stop() {
+  if [ -n "$timer_start" ]; then
+    local delta_us=$((($(timer_now) - $timer_start) / 1000))
+  else;
+    local delta_us=0
+  fi
+  local us=$((delta_us % 1000))
+  local ms=$(((delta_us / 1000) % 1000))
+  local s=$(((delta_us / 1000000) % 60))
+  local m=$(((delta_us / 60000000) % 60))
+  local h=$((delta_us / 3600000000))
+
+  if [ -n "$GDATE_EXISTS" ]; then
+    timer_show=$(printf "%02d:%02d.%03d" $m $s $ms)
+  else
+    timer_show=$(printf "%02d:%02d" $m $s)
+  fi
+
+  if ((h > 0)); then
+    timer_show=${h}:${timer_show}
+  fi
+
+  if ((delta_us < 1000000)); then timer_color=0  # 1s
+  elif ((delta_us < 5000000)); then timer_color=1 # 5s
+  elif ((delta_us < 30000000)); then timer_color=2 # 30s
+  elif ((delta_us < 300000000)); then timer_color=3 # 300s
+  else timer_color=4
+  fi
+
+  unset timer_start
+}
+
+elapsed() {
+  timer_stop
+  elapsed=""
+  if [[ $timer_color == 0 ]]; then
+    elapsed+="%{$fg_bold[white]%}"
+  elif [[ $timer_color == 1 ]]; then
+    elapsed+="%{$fg_bold[yellow]%}"
+  elif [[ $timer_color == 2 ]]; then
+    elapsed+="%{$fg_bold[cyan]%}"
+  elif [[ $timer_color == 3 ]]; then
+    elapsed+="%{$fg_bold[red]%}"
+  else
+    elapsed+="%{$bg_bold[red]%}"
+  fi
+  elapsed+="[$timer_show]%{$reset_color%}"
+  echo "$elapsed"
+}
+
+current_time() {
+  echo "%*"
+}
+
+set_iterm2_environment() {
+  export iterm2environment=$1
+  export iterm2environmentshort=$2
+# Having trouble getting this to work without shifting the git status RPROMPT over quite a bit
+#  printf "\033]1337;SetUserVar=%s=%s\007" iterm2environment `echo -n $iterm2environmentshort | base64`
+#  printf "\e]1337;SetBadgeFormat=%s\a" $(echo -n $iterm2environmentshort | base64)
+}
+#export -f set_iterm2_environment 1>/dev/null
+
+machine_name() {
+  shorthostname=$(sed 's/\([^\.]*\).*/\1/' <<< $(hostname))
+  if [[ $shorthostname == "Edwins-MBP" || $shorthostname == "Edwins-MBP-2" || $shorthostname == "Edwins-MacBook-Pro" ]]; then
+    shorthostname="macos"
+  fi
+  set_iterm2_environment "$shorthostname" $shorthostname
+  echo "%{$fg_bold[green]%}$shorthostname%{$reset_color%}"
+}
+
+export PROMPT=$'$(colorized_exit_code) $(elapsed) $(current_time) $(machine_name): $(directory_name)> '
 set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}%{$reset_color%}"
+  export RPROMPT="%{$fg_bold[cyan]%}$(git_dirty)$(need_push)%{$reset_color%}"
+}
+
+preexec() {
+  timer_start=$(timer_now)
 }
 
 precmd() {
+  LastExitCodeValue=$? # Must come first
   title "zsh" "%m" "%55<...<%~"
   set_prompt
 }
